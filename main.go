a844968b1e716ce3a11c3d19758d63ffdf2baafc
WhaleShip <andreipogirei@gmail.com>
2025-04-20 02:31:00 +0500

    repo init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	log.Fatal(app.Listen(":8080"))
+}
